e6aaf00b12e884825e4a6d4cc01b858b8216ef5e
kunjesh1 <kumarkunjesh1@gmail.com>
2019-07-03 18:23:04 +0530

    added the navigation panel

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component } from "@angular/core"
+import {Component,EventEmitter,Output } from "@angular/core"
 
 @Component({
 
@@ -7,6 +7,15 @@ templateUrl:'./header.component.html'
 })
 
 export class HeaderComponent{
+
+
+    @Output() featureSelected=new EventEmitter<string>();
+    onSelect(feature:string){
+
+        this.featureSelected.emit(feature);
+
+
+    }
 
 
 }
