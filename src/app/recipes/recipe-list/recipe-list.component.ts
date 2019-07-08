a479c91982c853846b165162cf0fb3f97fd9af91
Kunjesh Kumar <kumarkunjesh1@gmail.com>
2019-07-08 09:56:31 +0530

    added updated file

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 import {Recipe}  from '../recipe.model'
 import { EventEmitter } from '@angular/core';
@@ -10,10 +10,11 @@ import { EventEmitter } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
 
-  recipeWasSelected=new EventEmitter<Recipe>();
+ @Output() recipeWasSelected=new EventEmitter<Recipe>();
   recipes:Recipe[]=[
 
-    new Recipe('A test Recipe','This is simply a test','https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg')
+    new Recipe('A test Recipe','This is simply a test','https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg'),
+    new Recipe('A test Recipe','This is simply a test','https://static01.nyt.com/images/2019/01/17/dining/mc-red-lentil-soup/merlin_146234352_d7bc8486-b067-4cff-a4c0-7741f166fb60-articleLarge.jpg')
   ];
   constructor() { }
 
@@ -21,6 +22,7 @@ export class RecipeListComponent implements OnInit {
   }
 
   onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
 
 
   }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-import {Ingredient} from '../shared/ingredient.model'
-import { from } from 'rxjs';
-
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,22 +8,15 @@ import { from } from 'rxjs';
 })
 export class ShoppingListComponent implements OnInit {
 
-
-  ingredients:Ingredient[]=[
-
-    new Ingredient('Apples',5),
-    new Ingredient('Tomatoes',10),
-
-  ];
-  constructor() {
-
-
-
-   }
-
-
+  ingredients:Ingredient[]=[new Ingredient("Apple",5),new Ingredient("Mango",10) ];
+  constructor() { }
 
   ngOnInit() {
+  }
+
+  onIngredientAdded(ingredient:Ingredient){
+  this.ingredients.push(ingredient);
+
   }
 
 }
